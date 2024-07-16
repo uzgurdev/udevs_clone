1051f2914377286948afa3455aab02b5f2b863dc
HugoWebDev <mrhuman2709@gmail.com>
2024-07-16 15:50:13 +0500

    components moved

--- app/components/clients.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const Clients = (props: Props) => {
-  return <div>Clients</div>;
-};
-
-export default Clients;
---
+++ components/clients.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+type Props = {};
+
+const Clients = (props: Props) => {
+  return (
+    <div>
+      Clients Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum
+      officiis doloribus fugit, quo ullam repellat aut, quia omnis asperiores
+      maxime est, nulla ea alias beatae quibusdam quidem quis ut quasi? Sequi
+      sunt quis ullam suscipit fugit? Ab sed, accusantium tempore
+      exercitationem, qui quia repellat quasi, dolorem pariatur quos vel quam
+      quis commodi praesentium velit magnam ex voluptatem provident itaque ipsum
+      sapiente sunt? Ratione voluptates amet doloremque cupiditate, libero
+      maiores magni vero! Voluptatum consectetur nesciunt labore recusandae esse
+      nulla porro quis aspernatur nisi illo! Ratione nostrum dignissimos maiores
+      beatae minus dolor cupiditate. Amet ipsum eaque dolor? Delectus a modi
+      aspernatur nostrum ipsam eaque sunt! Tenetur blanditiis pariatur nostrum
+      fuga, tempora, officia dolor deleniti odit dolore facere nemo quaerat
+      error deserunt officiis aspernatur modi quisquam similique sapiente
+      inventore laboriosam sunt! Excepturi ea tempore iste perferendis aliquam.
+      Cum facilis amet dolorem minima laudantium culpa enim qui quam consequatur
+      necessitatibus ullam laborum autem id natus perspiciatis aliquid at
+      aliquam odit veniam eveniet, explicabo ratione illo porro. Aperiam sequi
+      sit officia omnis, assumenda, eligendi ab natus, soluta et perspiciatis
+      harum iusto? Ut corporis ad saepe earum dolores beatae veniam consectetur
+      quasi. Accusamus eos ullam quis esse aliquam natus corporis nam amet sit
+      quas ab ipsa quibusdam culpa quo deserunt eligendi, iure tenetur debitis,
+      ut ex inventore unde, libero necessitatibus. Quo commodi corrupti vel quia
+      consequatur expedita voluptatibus, illum molestiae placeat ea ad nostrum
+      qui assumenda rem, asperiores quam beatae minus molestias consequuntur
+      minima magni maxime eaque adipisci. Deserunt placeat adipisci sed velit
+      voluptas reiciendis nulla eum sint illum inventore. Dolor ab sit corporis
+      a, voluptas adipisci ex enim neque! Doloremque ipsa voluptatibus mollitia,
+      hic reiciendis, quod esse non id excepturi voluptate temporibus illum,
+      cupiditate blanditiis? Iste, quod neque! Incidunt, nostrum vitae dolorum
+      deserunt voluptate cupiditate molestias, totam assumenda earum aspernatur
+      consequatur nobis id accusantium magnam.
+    </div>
+  );
+};
+
+export default Clients;
